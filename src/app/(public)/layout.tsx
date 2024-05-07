0168e11b16255e3a42c774f02d1f1dbc7eb8c105
Ivo Junior <ivo@Ivos-MacBook-Pro.local>
2024-05-07 04:03:50 -0300

    add analyttics

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import '../globals.css';
 import { cn } from '@/lib/utils';
 import PublicProvider from '@/providers';
@@ -22,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
+        <Analytics />
         <PublicProvider>
           <div className="relative flex min-h-screen flex-col bg-background">
             <Navbar />
